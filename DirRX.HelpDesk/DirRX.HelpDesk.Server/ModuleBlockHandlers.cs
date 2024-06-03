2f82440204d75d0116a668919acf7ee971705a5b
DirectumRXUser <directumrxuser@c.ru>
2024-06-03 10:04:18 +0400

    Добавил св-ва в TypeRequest

--- DirRX.HelpDesk/DirRX.HelpDesk.Server/ModuleBlockHandlers.cs
+++ DirRX.HelpDesk/DirRX.HelpDesk.Server/ModuleBlockHandlers.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Sungero.Core;
 using Sungero.CoreEntities;
-using DirRX.HelpDesk.TypeRequest;
+using Sungero.Workflow;
 
-namespace DirRX.HelpDesk
+namespace DirRX.HelpDesk.Server.HelpDeskBlocks
 {
 }
